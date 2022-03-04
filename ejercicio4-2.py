15203aa2fe2a3e491cb7f07fe0da10b4efbc9ff7
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-04 16:46:30 +0100

    Ejercicio 4-2

---
+++ ejercicio4-2.py
@@ -0,0 +1,8 @@
+numero_inicial = 1
+numero_final = 25
+
+while numero_inicial <= numero_final:
+    if(numero_inicial % 2 != 0):
+        print("El numero: ", numero_inicial, " es impar")
+    numero_inicial += 1   
+    
