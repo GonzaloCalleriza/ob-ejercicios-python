8fa31d35a49ea6baf84a858948f3e939e5d2639e
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-04 13:14:14 +0100

    Ejercicio 3-1

---
+++ ejercicio3-1.py
@@ -0,0 +1,19 @@
+nombre = "Gonzalo"
+apellido = "Calleriza"
+edad = 33
+email = "gonzalo@email.com"
+telefono = 203203203
+casado = False
+hijos = True
+amigos = [ 'Pedro', 'Lucas', 'Juan']
+peliculas = { 'accion' : 'El Patriota', 'terror' : 'La casa en la Montaña', 'humor' : 'La pistola desnuda'}
+
+print(nombre)
+print(apellido)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(hijos)
+print(amigos)
+print(peliculas)
