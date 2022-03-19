b89de95ce8cbd6082fa6e6a0f13c740ffe39fbd2
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-19 19:37:38 +0100

    Ejercicio 7-1

---
+++ ejercicio7-1/main.py
@@ -0,0 +1,11 @@
+from operaciones import operador
+
+def main():
+    print(operador.suma(2,2))
+    print(operador.resta(2, 2))
+    print(operador.division(2, 2))
+    print(operador.multiplicacion(2, 2))
+
+
+if __name__ == '__main__':
+    main()
