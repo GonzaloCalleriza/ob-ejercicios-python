2af135df76c6b9fa533a4e94a300cfd6df6c1810
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-24 13:07:10 +0100

    Ejercicio 8-1

---
+++ ejercicio8-1.py
@@ -0,0 +1,7 @@
+# crear un archivo py donde creéis un archivo txt, lo abráis y escribáis dentro del archivo.
+# Para ello, tendréis que acceder dos veces al archivo creado.
+
+ejercicio = open('ejercicio8.txt', 'w')
+ejercicio.write("Aprendiendo a escribir en un archivo")
+ejercicio.close
+
